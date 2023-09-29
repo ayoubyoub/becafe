1cfefa1da0ce09c1ddfa78068dfc42cee52932f8
YOUB AYOUB <sis.ayoub.youb@gmail.com>
2023-09-29 22:53:13 +0100

    add lombok

--- src/main/java/com/becafe/model/Product.java
+++ src/main/java/com/becafe/model/Product.java
@@ -1,62 +1,33 @@
 package com.becafe.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private Long id;
+
+    @Getter
+    @Setter
     private String title;
+
+    @Getter
+    @Setter
     private String author;
+
+    @Getter
+    @Setter
     private String genre;
-
-    // Constructors, getters, and setters
-
-    // Default constructor (required by JPA)
-    public Product() {}
-
-    public Product(String title, String author, String genre) {
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
-    }
-
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
 }
--- target/generated-sources/annotations/com/becafe/security/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/becafe/security/mapper/UserMapperImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-29T22:20:28+0100",
+    date = "2023-09-29T22:53:01+0100",
     comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.35.0.v20230814-2020, environment: Java 17.0.8.1 (Eclipse Adoptium)"
 )
 public class UserMapperImpl implements UserMapper {
